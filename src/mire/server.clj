ebea0a40771971e48a5d9ab76e0d0e1fbd943029
ViktorKoltsov <90630784+BOOMAD@users.noreply.github.com>
2023-12-23 11:33:29 +0300

    Update server.clj

--- src/mire/server.clj
+++ src/mire/server.clj
@@ -28,7 +28,7 @@
 
     ;; We have to nest this in another binding call instead of using
     ;; the one above so *in* and *out* will be bound to the socket
-    (print "\nWhat is your name? ") (flush)
+    (print "What is your name?\n") (flush)
     (binding [player/*name* (get-unique-player-name (read-line))
               player/*current-room* (ref (@rooms/rooms :start))
               player/*inventory* (ref #{})]
